a1a13866b55c71d7909415813a46c0c132ff68d9
Jocastle98 <105643174+Jocastle98@users.noreply.github.com>
2025-02-17 10:01:57 +0900

    Update Timer.cs

--- C#_Function/Timer.cs
+++ C#_Function/Timer.cs
@@ -1,49 +1,39 @@
 
 public class JocastleTimer : MonoBehaviour
 {
-    [Serializable]
-    public class FillSettings
-    {
-        public Color color;
-    }
-
-    public FillSettings fillSettings;
-
-    [Serializable]
-    public class BackgroundSettings
-    {
-        public Color color;
-    }
-    public BackgroundSettings backgroundSettings;
-
     [SerializeField] private Image fillImage;
     [SerializeField] private float totalTime;
+    [SerializeField] private Image headCapImage;
+    [SerializeField] private Image tailCapImage;
     [SerializeField] private TMP_Text timeText;
+    
     
     public float CurrentImer{get; private set;}
     private bool _isPaused;
 
-    private float elapsedTime;  // 1초 체크용
     void Update()
     {
         if (!_isPaused)
         {
             CurrentImer += Time.deltaTime;
-            
-            fillImage.fillAmount = 0;
-            fillImage.fillAmount = CurrentImer / totalTime;
-            
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= 1f)
+            if (CurrentImer >= totalTime)
             {
-                elapsedTime = 0f;
-
-                float remainTime = totalTime - CurrentImer;
-                if (remainTime < 0f) remainTime = 0f;
-
-                // 1초 단위 정수 감소
-                timeText.text = Mathf.CeilToInt(remainTime).ToString();
+                headCapImage.gameObject.SetActive(false);
+                tailCapImage.gameObject.SetActive(false);
+                _isPaused = true;
             }
+            else
+            {
+                fillImage.fillAmount = (totalTime- CurrentImer) / totalTime;
+                headCapImage.transform.localRotation 
+                    = Quaternion.Euler(new Vector3(0,0,fillImage.fillAmount * 360));
+                
+                
+                var timeTextTime = totalTime - CurrentImer;
+                //F0: 소수 점 자리 표현 x
+                timeText.text = timeTextTime.ToString("F0");
+            }
+          
         }
     }
     
